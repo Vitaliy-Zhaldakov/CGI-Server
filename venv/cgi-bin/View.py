86c316d327358a22bc7125b2668ccd0d7386cad9
Vitaliy-Zhaldakov <78874453+Vitaliy-Zhaldakov@users.noreply.github.com>
2021-12-01 19:41:45 +0300

    Update forms, Add queries

--- venv/cgi-bin/View.py
+++ venv/cgi-bin/View.py
@@ -15,7 +15,8 @@ form = cgi.FieldStorage()
 
 # Получение данных из полей
 table = form.getvalue('Таблицы')
-addTable = form.getvalue('table')
+button = form.getvalue('Button')
+query = form.getvalue('Queries')
 
 print ("Content-type:text/html\r\n\r\n")
 print ("<html>")
@@ -23,7 +24,7 @@ print ("<head>")
 print ("<meta charset=""UTF-8"">")
 print ("<title>Фото-видео техника</title>")
 print ("</head>")
-print ("<body bgcolor = #7FFFD4 background = url(picture.jpg)>")
+print ("<body bgcolor = #7FFFD4>")
 print ("<h1 align = 'center'>Фото-видео техника</h1>")
 
 # Форма выбора таблиц для вывода
@@ -35,132 +36,151 @@ print("""<form action = "/cgi-bin/View.py" method = "post">
 <option value = "type">Виды фотоаппаратов</option>
 <option value = "maxResolution">Разрешения</option>
 </select>
-<input type = "submit" value = "Вывести" />
+<input type = "submit" name = "Button" value = "Вывести">
+<input type = "submit" name = "Button" value = "Добавить данные">
 </form>""")
 
-# Форма выбора таблиц для добавления данных
+# Форма для выбора запроса
 print("""<form action = "/cgi-bin/View.py" method = "post">
-<input type = 'radio' name = 'table' value = 'cameras' /> Фотоаппараты <br>
-<input type = 'radio' name = 'table' value = 'videoCameras' /> Видеокамеры <br>
-<input type = 'radio' name = 'table' value = 'actionCameras' /> Экшн-камеры <br>
-<input type = 'radio' name = 'table' value = 'maxResolution' /> Разрешения <br><br>
-<input type = "submit" value = "Добавить данные" />
-</form>""")
+<select name = "Queries">
+<option value = "onPrice">Устройства, по цене не превышающие 10 000 ₽</option>
+<option value = "highRes">Камеры с разрешением 1920х1080</option>
+<option value = "instPrintCameras">Фотоаппараты моментальной печати</option>
+</select>
+<input type = "submit" value = "Запросить">
+</form><br><br>""")
 
 # Вывод таблиц на экран
-if table == 'cameras':
-    # Запрос таблицы Фотоаппараты
-    cursor.execute("""SELECT cameraID, manufacturer, model, typeName, price
-                   FROM cameras JOIN type ON cameras.typeID = type.typeID""")
-    cameras = cursor.fetchall()
+if button == 'Вывести':
+    if table == 'cameras':
+        # Запрос таблицы Фотоаппараты
+        cursor.execute("""SELECT cameraID, manufacturer, model, typeName, price
+                       FROM cameras JOIN type ON cameras.typeID = type.typeID""")
+        cameras = cursor.fetchall()
 
-    # Таблица Фотоаппараты
-    print("<table cellspacing = '10'>"
-          "<caption><h2>Фотоаппараты</h2></caption>"
-          "<tr>"
-          "<th>ID</th>"
-          "<th>Производитель</th>"
-          "<th>Модель</th>"
-          "<th>Тип</th>"
-          "<th>Цена</th>"
-          "</tr>")
-    for row in cameras:
-        print("<tr><td align = 'center'>", row[0], "</td><td align = 'center'>", row[1],
-              "</td><td align = 'center'>", row[2], "</td><td align = 'center'>", row[3],
-              "</td><td align = 'center'>", row[4], "₽" "</td></tr>")
-    print("</table>")
+        # Таблица Фотоаппараты
+        print("<table cellspacing = '10'>"
+              "<caption><h2>Фотоаппараты</h2></caption>"
+              "<tr>"
+              "<th>ID</th>"
+              "<th>Производитель</th>"
+              "<th>Модель</th>"
+              "<th>Тип</th>"
+              "<th>Цена</th>"
+              "</tr>")
+        for row in cameras:
+            print("<tr><td align = 'center'>", row[0], "</td><td align = 'center'>", row[1],
+                  "</td><td align = 'center'>", row[2], "</td><td align = 'center'>", row[3],
+                  "</td><td align = 'center'>", row[4], "₽" "</td></tr>")
+        print("</table>")
 
-elif table == 'videoCameras':
-    # Запрос таблицы Видеокамеры
-    cursor.execute("""SELECT videoCamID, manufacturer, model, resolution, price
-                       FROM videoCameras JOIN maxResolution ON videoCameras.resolutionID = maxResolution.resolutionID""")
-    videoCams = cursor.fetchall()
+    elif table == 'videoCameras':
+        # Запрос таблицы Видеокамеры
+        cursor.execute("""SELECT videoCamID, manufacturer, model, resolution, price
+                           FROM videoCameras JOIN maxResolution ON videoCameras.resolutionID = maxResolution.resolutionID""")
+        videoCams = cursor.fetchall()
 
-    # Таблица Видеокамеры
-    print("<table cellspacing = '10'>"
-          "<caption><h2>Видеокамеры</h2></caption>"
-          "<tr>"
-          "<th>ID</th>"
-          "<th>Производитель</th>"
-          "<th>Модель</th>"
-          "<th>Разрешение</th>"
-          "<th>Цена</th>"
-          "</tr>")
-    for row in videoCams:
-        print("<tr><td align = 'center'>", row[0], "</td><td align = 'center'>", row[1],
-              "</td><td align = 'center'>", row[2], "</td><td align = 'center'>", row[3],
-              "</td><td align = 'center'>", row[4], "₽" "</td></tr>")
-    print("</table>")
+        # Таблица Видеокамеры
+        print("<table cellspacing = '10'>"
+              "<caption><h2>Видеокамеры</h2></caption>"
+              "<tr>"
+              "<th>ID</th>"
+              "<th>Производитель</th>"
+              "<th>Модель</th>"
+              "<th>Разрешение</th>"
+              "<th>Цена</th>"
+              "</tr>")
+        for row in videoCams:
+            print("<tr><td align = 'center'>", row[0], "</td><td align = 'center'>", row[1],
+                  "</td><td align = 'center'>", row[2], "</td><td align = 'center'>", row[3],
+                  "</td><td align = 'center'>", row[4], "₽" "</td></tr>")
+        print("</table>")
 
-elif table == 'actionCameras':
-    # Запрос таблицы Экшн-камеры
-    cursor.execute("""SELECT actionCamID, manufacturer, model, resolution, price
-                    FROM actionCameras JOIN maxResolution ON actionCameras.resolutionID = maxResolution.resolutionID""")
-    actionCams = cursor.fetchall()
+    elif table == 'actionCameras':
+        # Запрос таблицы Экшн-камеры
+        cursor.execute("""SELECT actionCamID, manufacturer, model, resolution, price
+                        FROM actionCameras JOIN maxResolution ON actionCameras.resolutionID = maxResolution.resolutionID""")
+        actionCams = cursor.fetchall()
 
-    # Таблица Экшн-камеры
-    print("<table cellspacing = '10'>"
-          "<caption><h2>Экшн-камеры</h2></caption>"
-          "<tr>"
-          "<th>ID</th>"
-          "<th>Производитель</th>"
-          "<th>Модель</th>"
-          "<th>Разрешение</th>"
-          "<th>Цена</th>"
-          "</tr>")
-    for row in actionCams:
-        print("<tr><td align = 'center'>", row[0], "</td><td align = 'center'>", row[1],
-              "</td><td align = 'center'>", row[2], "</td><td align = 'center'>", row[3],
-              "</td><td align = 'center'>", row[4], "₽" "</td></tr>")
-    print("</table>")
+        # Таблица Экшн-камеры
+        print("<table cellspacing = '10'>"
+              "<caption><h2>Экшн-камеры</h2></caption>"
+              "<tr>"
+              "<th>ID</th>"
+              "<th>Производитель</th>"
+              "<th>Модель</th>"
+              "<th>Разрешение</th>"
+              "<th>Цена</th>"
+              "</tr>")
+        for row in actionCams:
+            print("<tr><td align = 'center'>", row[0], "</td><td align = 'center'>", row[1],
+                  "</td><td align = 'center'>", row[2], "</td><td align = 'center'>", row[3],
+                  "</td><td align = 'center'>", row[4], "₽" "</td></tr>")
+        print("</table>")
 
-elif table == 'type':
-    # Запрос таблицы Типы
-    cursor.execute("""SELECT typeID, typeName
-                    FROM type""")
-    types = cursor.fetchall()
+    elif table == 'type':
+        # Запрос таблицы Типы фотоаппаратов
+        cursor.execute("""SELECT typeID, typeName
+                        FROM type""")
+        types = cursor.fetchall()
 
-    # Таблица Виды фотоаппаратов
-    print("<table cellspacing = '10'>"
-          "<caption><h2>Виды фотоаппаратов</h2></caption>"
-          "<tr>"
-          "<th>ID</th>"
-          "<th>Наименования</th>"
-          "</tr>")
-    for row in types:
-        print("<tr><td align = 'center'>", row[0], "</td><td align = 'center'>", row[1])
-    print("</table>")
+        # Таблица Виды фотоаппаратов
+        print("<table cellspacing = '10'>"
+              "<caption><h2>Виды фотоаппаратов</h2></caption>"
+              "<tr>"
+              "<th>ID</th>"
+              "<th>Наименования</th>"
+              "</tr>")
+        for row in types:
+            print("<tr><td align = 'center'>", row[0], "</td><td align = 'center'>", row[1])
+        print("</table>")
 
-elif table == 'maxResolution':
-    # Запрос таблицы Разрешения
-    cursor.execute("""SELECT resolutionID, resolution
-                        FROM maxResolution""")
-    resolutions = cursor.fetchall()
+    elif table == 'maxResolution':
+        # Запрос таблицы Разрешения
+        cursor.execute("""SELECT resolutionID, resolution
+                            FROM maxResolution""")
+        resolutions = cursor.fetchall()
 
-    # Таблица Разрешения
-    print("<table cellspacing = '10'>"
-          "<caption><h2>Максимальные разрешения</h2></caption>"
-          "<tr>"
-          "<th>ID</th>"
-          "<th>Разрешения</th>"
-          "</tr>")
-    for row in resolutions:
-        print("<tr><td align = 'center'>", row[0], "</td><td align = 'center'>", row[1])
-    print("</table>")
+        # Таблица Разрешения
+        print("<table cellspacing = '10'>"
+              "<caption><h2>Максимальные разрешения</h2></caption>"
+              "<tr>"
+              "<th>ID</th>"
+              "<th>Разрешения</th>"
+              "</tr>")
+        for row in resolutions:
+            print("<tr><td align = 'center'>", row[0], "</td><td align = 'center'>", row[1])
+        print("</table>")
 
-# Добавление данных в таблицы (просто формочка, когда сюда заходит сначала выполнялся наш запрос,
-# а уже потом с формочек читались данные, которые вводили
-if addTable == 'cameras':
-    print("""<form action = "/cgi-bin/View.py" method = "post">
-    Производитель: <input type = "text" name = "manufacturer" /> 
-    Модель: <input type = "text" name = "model" />
-    Тип фотоаппарата: <input type = "text" name = "type" />
-    Цена: <input type = "text" name = "price" />
-    <input type = 'submit' value = 'Добавить'>
-    </form>""")
+# Формы для добавления данных в таблицы
+if button == 'Добавить данные':
+    if table == 'cameras':
+        print("""<form action = "/cgi-bin/View.py" method = "post">
+        Производитель: <input type = "text" name = "manufacturer" /> 
+        Модель: <input type = "text" name = "model" />
+        Тип фотоаппарата: <input type = "text" name = "type" />
+        Цена: <input type = "text" name = "price" />
+        <input type = 'submit' value = 'Добавить'>
+        </form>""")
 
-elif addTable == 'videoCameras':
-    print("""<form action = "/cgi-bin/View.py" method = "post">
+        # Запрос таблицы Типы фотоаппаратов
+        cursor.execute("""SELECT typeID, typeName
+                                FROM type""")
+        types = cursor.fetchall()
+
+        # Таблица Виды фотоаппаратов
+        print("<table cellspacing = '10'>"
+              "<caption><h2>Виды фотоаппаратов</h2></caption>"
+              "<tr>"
+              "<th>ID</th>"
+              "<th>Наименования</th>"
+              "</tr>")
+        for row in types:
+            print("<tr><td align = 'center'>", row[0], "</td><td align = 'center'>", row[1])
+        print("</table>")
+
+    elif table == 'videoCameras':
+        print("""<form action = "/cgi-bin/View.py" method = "post">
         Производитель: <input type = "text" name = "manufacturer" /> 
         Модель: <input type = "text" name = "model" />
         Разрешение: <input type = "text" name = "resolutionID" />
@@ -168,7 +188,23 @@ elif addTable == 'videoCameras':
         <input type = 'submit' value = 'Добавить'>
         </form>""")
 
-# Получение данных из полей
+        # Запрос таблицы Разрешения
+        cursor.execute("""SELECT resolutionID, resolution
+                                    FROM maxResolution""")
+        resolutions = cursor.fetchall()
+
+        # Таблица Разрешения
+        print("<table cellspacing = '10'>"
+              "<caption><h2>Максимальные разрешения</h2></caption>"
+              "<tr>"
+              "<th>ID</th>"
+              "<th>Разрешения</th>"
+              "</tr>")
+        for row in resolutions:
+            print("<tr><td align = 'center'>", row[0], "</td><td align = 'center'>", row[1])
+        print("</table>")
+
+# Получение данных из полей добавления
 manufacturer = form.getvalue('manufacturer')
 model = form.getvalue('model')
 typeCam = form.getvalue('type')
@@ -186,9 +222,96 @@ if resolution:
                    (manufacturer, model, resolution, price))
     connection.commit()
 
+# Выполнение запросов
+if query == 'onPrice':
+
+    # Запрос к таблице Фотоаппараты
+    cursor.execute("""SELECT manufacturer, model, typeName, price
+                    FROM cameras JOIN type ON cameras.typeID = type.typeID
+                    WHERE price <= 10000""")
+    cameras = cursor.fetchall()
+
+    # Запрос к таблицам Видеокамеры и Экшн-камеры
+    cursor.execute("""SELECT manufacturer, model, resolution, price
+                        FROM videoCameras JOIN maxResolution ON videoCameras.resolutionID = maxResolution.resolutionID
+                        WHERE price <= 10000""")
+    videoCameras = cursor.fetchall()
+
+    cursor.execute("""SELECT manufacturer, model, resolution, price
+                    FROM actionCameras JOIN maxResolution ON actionCameras.resolutionID = maxResolution.resolutionID
+                    WHERE price <= 10000""")
+    actionCameras = cursor.fetchall()
+
+    vidActCameras = videoCameras + actionCameras
+
+    # Отрисовка таблицы
+    print("<table cellspacing = '10'>"
+          "<caption><h2>Устройства, по цене не превышающие 10 000 ₽</h2></caption>"
+          "<tr>"
+          "<th>Производитель</th>"
+          "<th>Модель</th>"
+          "<th>Тип</th>"
+          "<th>Разрешение</th>"
+          "<th>Цена</th>"
+          "</tr>")
+    for row in cameras:
+        print("<tr><td align = 'center'>", row[0], "</td><td align = 'center'>", row[1],
+              "</td><td align = 'center'>", row[2], "</td><td align = 'center'>", '-',
+              "</td><td align = 'center'>", row[3], "₽" "</td></tr>")
+    for row in vidActCameras:
+        print("<tr><td align = 'center'>", row[0], "</td><td align = 'center'>", row[1],
+              "</td><td align = 'center'>", '-', "</td><td align = 'center'>", row[2],
+              "</td><td align = 'center'>", row[3], "₽" "</td></tr>")
+    print("</table>")
+
+if query == 'highRes':
+
+    # Запрос к таблицам Видеокамеры и Экшн-камеры
+    cursor.execute("""SELECT manufacturer, model, price
+                            FROM videoCameras JOIN maxResolution ON videoCameras.resolutionID = maxResolution.resolutionID
+                            WHERE resolution = '1920x1080'""")
+    videoCameras = cursor.fetchall()
+
+    cursor.execute("""SELECT manufacturer, model, price
+                        FROM actionCameras JOIN maxResolution ON actionCameras.resolutionID = maxResolution.resolutionID
+                        WHERE resolution = '1920x1080'""")
+    actionCameras = cursor.fetchall()
+
+    vidActCameras = videoCameras + actionCameras
+
+    # Отрисовка таблицы
+    print("<table cellspacing = '10'>"
+          "<caption><h2>Камеры с разрешением 1920х1080</h2></caption>"
+          "<tr>"
+          "<th>Производитель</th>"
+          "<th>Модель</th>"
+          "<th>Цена</th>"
+          "</tr>")
+    for row in vidActCameras:
+        print("<tr><td align = 'center'>", row[0], "</td><td align = 'center'>", row[1],
+            "</td><td align = 'center'>", row[2], "₽" "</td></tr>")
+    print("</table>")
+
+if query == 'instPrintCameras':
+
+    # Запрос к таблице Фотоаппараты
+    cursor.execute("""SELECT manufacturer, model, price
+                        FROM cameras JOIN type ON cameras.typeID = type.typeID
+                        WHERE typeName = 'Instant printing camera'""")
+    cameras = cursor.fetchall()
+
+    # Отрисовка таблицы
+    print("<table cellspacing = '10'>"
+          "<caption><h2>Фотоаппараты моментальной печати</h2></caption>"
+          "<tr>"
+          "<th>Производитель</th>"
+          "<th>Модель</th>"
+          "<th>Цена</th>"
+          "</tr>")
+    for row in cameras:
+        print("<tr><td align = 'center'>", row[0], "</td><td align = 'center'>", row[1],
+              "</td><td align = 'center'>", row[2], "₽" "</td></tr>")
+    print("</table>")
+
 print ("</body>")
 print ("</html>")
-
-# Nikon, D3500 Kit 18-55mm VR AF-P, 5, 39999
-
-# Sony, FDR-AX700, 7, 124990
